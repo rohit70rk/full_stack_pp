5408a67a9b7fd83394c6182cd6973a3e3cd1cbd6
Rohit Kumar <rohitkumar81943@gmail.com>
2025-09-22 18:24:24 +0530

    make responsive to mobile view

--- fspp/routes.py
+++ fspp/routes.py
@@ -9,7 +9,19 @@ def home():
 def projects():
     return render_template('projects.html')  # must match file name in templates folder
 
-
 @app.route("/project/<int:project_id>")
 def project_detail(project_id):
+    # This should be updated later to show a real project detail page
     return render_template('registration_form.html')
+
+# --- NEW ROUTES TO LINK YOUR PAGES ---
+@app.route("/experience")
+def experience():
+    """Renders the experience page."""
+    return render_template('experience.html')
+
+@app.route("/blog")
+def blog():
+    """Renders the blog page."""
+    return render_template('blog.html')
+
