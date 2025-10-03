00f816462752f350d20527722ac501424dd3d232
Rohit Kumar <rohitkumar81943@gmail.com>
2025-10-03 20:10:31 +0530

    stucture new project

--- fspp/routes.py
+++ fspp/routes.py
@@ -1,13 +1,51 @@
 from flask import render_template
 from fspp import app
 
+# --- CENTRALIZED PROJECT DATA ---
+# This is now the only place you need to add, edit, or remove a project.
+projects_data = [
+    {
+        'id': 1,
+        'title': 'Project 1 Title',
+        'description': 'A short, engaging description of this project and the tech used.',
+    },
+    {
+        'id': 2,
+        'title': 'Project 2 Title',
+        'description': 'A short, engaging description of this project and the tech used.',
+    },
+    {
+        'id': 3,
+        'title': 'Project 3 Title',
+        'description': 'A short, engaging description of this project and the tech used.',
+    },
+    {
+        'id': 4,
+        'title': 'Project 4 Title',
+        'description': 'A short, engaging description of this project and the tech used.',
+    },
+    {
+        'id': 5,
+        'title': 'Project 5 Title',
+        'description': 'A short, engaging description of this project and the tech used.',
+    },
+    {
+        'id': 6,
+        'title': 'Project 6: New Project',
+        'description': 'A short, engaging description of this project and the tech used.',
+    }
+]
+
+
 @app.route("/")
 def home():
-    return render_template('index.html')
+    # Pass the first 5 projects to the homepage preview
+    return render_template('index.html', projects=projects_data[:5])
 
 @app.route("/projects")
 def projects():
-    return render_template('projects.html')  # must match file name in templates folder
+    # Pass all projects to the projects page
+    return render_template('projects.html', projects=projects_data)
 
 @app.route("/project/<int:project_id>")
 def project_detail(project_id):
@@ -24,4 +62,3 @@ def experience():
 def blog():
     """Renders the blog page."""
     return render_template('blog.html')
-
