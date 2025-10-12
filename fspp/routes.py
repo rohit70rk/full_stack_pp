6b8930313376d4b62225a2536740e7463442736f
Rohit Kumar <rohitkumar81943@gmail.com>
2025-10-12 18:47:11 +0530

    create new project Live Webcam Viewer

--- fspp/routes.py
+++ fspp/routes.py
@@ -8,8 +8,9 @@ projects_data = [
     {'id': 1, 'title': 'Embedded Google Form', 'description': 'A demonstration of embedding a live Google Form into a webpage.', 'has_detail': True, 'detail_route': 'google_form.details'},
     # NEW: This is your second project (the clone)
     {'id': 2, 'title': 'Google Form Clone', 'description': 'A project to replicate the functionality and style of a Google Form.', 'has_detail': True, 'detail_route': 'google_form_clone.details'},
-    
-    {'id': 3, 'title': 'Project 3 Title', 'description': 'A short, engaging description of this project and the tech used.', 'has_detail': False},
+    # NEW: Register the webcam project
+    {'id': 3, 'title': 'Live Webcam Viewer', 'description': 'Access and display a live webcam feed in the browser using Flask and JavaScript.', 'has_detail': True, 'detail_route': 'live_webcam.details'},
+
     {'id': 4, 'title': 'Project 4 Title', 'description': 'A short, engaging description of this project and the tech used.', 'has_detail': False},
     {'id': 5, 'title': 'Project 5 Title', 'description': 'A short, engaging description of this project and the tech used.', 'has_detail': False},
     {'id': 6, 'title': 'Project 6 Title', 'description': 'A short, engaging description of this project and the tech used.', 'has_detail': False}
